357064ee939357ab7c3d37e7b6237c89d2edbc38
raj <mailmeraj23@gmail.com>
2023-09-04 19:15:42 -0500

    errorin list viw

--- myblog/main/urls.py
+++ myblog/main/urls.py
@@ -4,6 +4,8 @@ urlpatterns=[
    
     path('',views.index,name= 'index'),
     path('atlas',views.Atlas.as_view()),
+    path('college/<int:pk>',views.CollegeDetail.as_view(),name= "college" ),
+    path('college',views.CollegeList.as_view()),
     path('forms',views.formchecker,name= 'formchecker'),
     path('article/<int:pk>',views.article, name='get_article'),
     path('author/<int:pk>',views.author, name='get_author'),
